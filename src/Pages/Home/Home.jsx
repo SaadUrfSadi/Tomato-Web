2e7ffe13123e912748e6077e39aa571aae510a86
Your Name <urfsaadsai10@gmail.com>
2025-08-23 19:39:34 +0500

    Initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import "./Home.css";
+import Header from '../../Components/Header/Header';
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
+
+function Home() {
+  const [category, setCategory] = useState("All")
+  return (
+    <>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </>
+  )
+}
+
+export default Home
