0152f57f4d5da1e2daf97cc80d577780aacef036
Your Name <urfsaadsai10@gmail.com>
2025-08-28 00:14:51 +0500

    updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/frontend_assets/assets";
 import {Link} from 'react-router-dom';
+import { StoreContext } from "../../Context/StoreContext";
 function Navbar({setShowLogin}) {
-  //   console.log(assets);
+  
   const [menu, setMenu] = useState("home");
+
+  const {getTotalCartAmount} = useContext(StoreContext);
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+     <Link to="/"><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar_menu">
         <Link
           to="/"
@@ -41,8 +44,8 @@ function Navbar({setShowLogin}) {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=> setShowLogin(true)}>Signin</button>
       </div>
--- src/Pages/PlaceOrder/PlaceOrder.jsx
+++ src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./PlaceOrder.css";
 
 function PlaceOrder() {
   return (
-    <div>PlaceOrder</div>
+    <>
+    <div className="place-order">
+      
+    </div>
+    </>
   )
 }
 
